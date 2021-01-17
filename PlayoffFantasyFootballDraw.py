40d7bfa0f0472c85cc7ef63763259b1c0d393503
Hadar Grubman <hagrubma@microsoft.com>
2021-01-17 23:18:18 +0200

    SAVEPOINT-master

--- PlayoffFantasyFootballDraw.py
+++ PlayoffFantasyFootballDraw.py
@@ -3,16 +3,15 @@ import sys
 import time
 
 players = [
-    "Adi",
-    "Ayal",
-    "Daniel",
-    "Ehud",
-    "Felix",
+    "Amihay",
     "Gilad",
     "Hadar",
-    "Jean",
+    "Ido",
+    "Ofer",
+    "Omer",
     "On",
     "Ophir",
+    "Shachar",
     "Tom"
 ]
 
@@ -25,10 +24,10 @@ for index in range(numOfPlayers):
     sys.stdout.write("Pick number {} goes to ".format(backIndex+1))
     sys.stdout.flush()
     for wait in range(3):
-        time.sleep(3)
+        time.sleep(1)
         sys.stdout.write(".")
         sys.stdout.flush()
-    time.sleep(3)
+    time.sleep(1)
     sys.stdout.write(" {}\n".format(players[backIndex]))
     sys.stdout.flush()
 
