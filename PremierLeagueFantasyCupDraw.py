e3224e021eb9087e2a7f0d169d97ee8df23197c5
Hadar Grubman <hagrubma@microsoft.com>
2022-08-05 18:11:44 +0300

    SAVEPOINT-master

--- PremierLeagueFantasyCupDraw.py
+++ PremierLeagueFantasyCupDraw.py
@@ -3,83 +3,45 @@ import sys
 import time
 
 players = [
-    'Blue4ever (Adi Ginzburg)',
-    'Red or dead (Kfir Emmer)',
-    'Atias (Dudi Atias)',
-    'Roe ikeda (Roe Ikeda)',
-    'Hazans (Dor Hazan)',
-    'Show Me The Mane (Ehud Gershon)',
-    'Danny Shaw-ve-Ivan-y (Hadar Grubman)',
-    'Shaw Mee De Mane (David Peres)',
-    'Mandelim (Ohad Mandel)',
-    'Yellowmania (Yaniv Salomon)',
-    'Gg! (Michael Peleg)',
-    'Mkharbatyan (Alon Shamir)',
-    'Wan Man Shaw (Ronen Pari)',
-    'Boom Shaka Saka (Tal Avraham)',
-    'OT FC (Attar Tur)',
-    'HTA (Daniel Zilberberg)',
-    'Borussia Shkolar (Yuval Shkolar)',
-    'Hapo hell (Yoav Mor)',
-    'GrealishChildish (Or Portal)',
-    'The Larks (Niv Yosefi)',
-    'Charlie and a half (Idan Even)',
-    'Harrison Lord (Tom Harel)',
-    'cococoufal (Daniel Simon)',
-    'baruch sarusi (Baruch Sarusi)',
-    'F.C Trentola (Karel Poborsky)',
-    'It Almost Came Home (Alex Golts)',
-    'Saka Avocado (Alon Meir)',
-    'AbraDubravka (Rani Eylat)',
-    'Moura bora (Tomer Goldthorpe)',
-    'FORZA GUNNERS (Elad Kainer)'
-]
-
-round_of_16 = [
-    'Winner 1',
-    'Winner 2',
-    'Winner 3',
-    'Winner 4',
-    'Winner 5',
-    'Winner 6',
-    'Winner 7',
-    'Winner 8',
-    'Winner 9',
-    'Winner 10',
-    'Winner 11',
-    'Winner 12',
-    'Winner 13',
-    'Winner 14',
-    'Winner 15',
-    'Best looser'
-]
-
-quarter_finals = [
-    'Winner 16',
-    'Winner 17',
-    'Winner 18',
-    'Winner 19',
-    'Winner 20',
-    'Winner 21',
-    'Winner 22',
-    'Winner 23'
-]
-
-semi_finals = [
-    'Winner 24',
-    'Winner 25',
-    'Winner 26',
-    'Winner 27'
-]
-
-final = [
-    'Winner 28',
-    'Winner 29'
+    'Hadar Grubman',
+    'Kfir Emmer',
+    'Daniel Zilberberg',
+    'Bar Badihi',
+    'Ofer Nikos Tsitiat',
+    'hadar hason‬‏',
+    'Michael Peleg',
+    'Or Portal',
+    'Omer Apel‬‏',
+    'Dani Shafir',
+    'David Cohen',
+    'Idan Shwartz',
+    'Yuval Shkolar',
+    'Baruch Sarusi',
+    'Dudi Atias',
+    'Idan Even',
+    'Adi Ginzburg',
+    'Yogev Balilti',
+    'Dor Hazan',
+    'Alon Meir',
+    'Oran Turgeman',
+    'Tomer Goldthorpe',
+    'Alex Golts',
+    'Elad Kainer',
+    'Roe Ikeda',
+    'Rani Eylat',
+    'Yaniv Salomon',
+    'Daniel Simon',
+    'Tal Avraham',
+    'Alon Shamir',
+    'Ohad Mandel',
+    'Tom Harel',
+    'David Peres',
+    'Ronen Pari'
 ]
 
 
 def print_same_line(prefix, suffix):
-    sleep_time = 1
+    sleep_time = 0
     sys.stdout.write(prefix)
     sys.stdout.flush()
     for wait in range(5):
@@ -96,14 +58,14 @@ def print_line_no_delay(line):
     sys.stdout.flush()
 
 
-def shuffle_stage_and_print(stage_name, stage_players, first_match):
+def shuffle_stage_and_print(stage_name, stage_players, first_match, match_week):
     for index in range(5000):
         random.shuffle(stage_players)
 
-    print_line_no_delay('\n{}:\n'.format(stage_name))
+    print_line_no_delay('\n{}: (Match Week {})\n'.format(stage_name, match_week))
     for index in range(len(stage_players)):
         if index % 2 == 0:
-            match_number = (index/2) + first_match
+            match_number = int((index/2) + first_match)
             if match_number > 9:
                 spaces = '   '
             else:
@@ -113,12 +75,38 @@ def shuffle_stage_and_print(stage_name, stage_players, first_match):
             print_same_line(' - ', '{}\n'.format(stage_players[index]))
 
 
+def shuffle_subgroup(original_group, subgroup_size):
+    group = []
+    group.extend(original_group)
+
+    for index in range(5000):
+        random.shuffle(group)
+
+    return group[0:subgroup_size], group[subgroup_size:]
+
+
+def generate_winners_list(first_winner_index, winners_count):
+    winners = []
+    for index in range(winners_count):
+        winners.append('Winner ' + str(index + first_winner_index))
+
+    return winners
+
+
 def main():
-    shuffle_stage_and_print('First Round', players, 1)
-    shuffle_stage_and_print('Round of 16', round_of_16, 16)
-    shuffle_stage_and_print('Quarter Finals', quarter_finals, 24)
-    shuffle_stage_and_print('Semi Finals:', semi_finals, 28)
-    shuffle_stage_and_print('Final', final, 30)
+    (preliminary_round_players, rest_of_players) = shuffle_subgroup(players, 4)
+    rest_of_players.extend(generate_winners_list(1, 2))
+
+    preliminary_round_game_week = shuffle_subgroup(range(10, 17), 1)[0][0]
+    round_game_weeks = shuffle_subgroup(range(17, 38), 5)[0]
+    round_game_weeks.sort()
+
+    shuffle_stage_and_print('Preliminary Round', preliminary_round_players, 1, preliminary_round_game_week)
+    shuffle_stage_and_print('First Round', rest_of_players, 3, round_game_weeks[0])
+    shuffle_stage_and_print('Round of 16', generate_winners_list(3, 16), 19, round_game_weeks[1])
+    shuffle_stage_and_print('Quarter Finals', generate_winners_list(19, 8), 27, round_game_weeks[2])
+    shuffle_stage_and_print('Semi Finals:', generate_winners_list(27, 4), 31, round_game_weeks[3])
+    shuffle_stage_and_print('Final', generate_winners_list(31, 2), 33, round_game_weeks[4])
 
 
 if __name__ == "__main__":
