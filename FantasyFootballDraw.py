1c225266fdfe8bf81c69d0f65d9d83717ac548dc
Hadar Grubman <hagrubma@microsoft.com>
2023-07-29 21:53:49 +0300

    SAVEPOINT-master

--- FantasyFootballDraw.py
+++ FantasyFootballDraw.py
@@ -22,7 +22,7 @@ divisions = [
 
 
 def print_same_line(prefix, suffix):
-    sleep_time = 1
+    sleep_time = 0
     sys.stdout.write(prefix)
     sys.stdout.flush()
     for wait in range(3):
@@ -51,55 +51,26 @@ def print_results():
         print_new_line('Pick number {} goes to '.format(back_index + 1), ' {}'.format(players[back_index]))
 
 
-def validate_number(input_str, name, min_value, max_value):
+def validate_number(input_str):
     if not input_str.isdigit():
-        print('{} should be a number'.format(name))
+        print('The pick should be a number')
         sys.exit(1)
 
     value = int(input_str)
-    if value < min_value or value > max_value:
-        print('{} should be between {} and {}'.format(name, min_value, max_value))
+    if value < 1 or value > 10:
+        print('The pick should be between 1 and 10')
         sys.exit(1)
 
     return value
 
 
 def validate_raw_input(arguments):
-    consolation_prize_winner = arguments[0]
+    consolation_prize_winner = arguments[1]
     if consolation_prize_winner not in players:
         print('ConsolationPrizeWinner should be one of {players}'.format(players=', '.join(players)))
         sys.exit(1)
 
-    input_name = 'onlyChoice' if len(arguments) == 3 else 'FirstChoice'
-    min_value = 5 if len(arguments) == 2 else 1
-    consolation_prize_winner_first_pick = validate_number(arguments[1], input_name, min_value, max_value=10)
-
-    consolation_prize_winner_second_pick = 0
-    if len(arguments) == 3:
-        consolation_prize_winner_second_pick = validate_number(arguments[2], 'SecondChoice', min_value=1, max_value=10)
-
-    return consolation_prize_winner, consolation_prize_winner_first_pick, consolation_prize_winner_second_pick
-
-
-def consolation_prize_draw(array_size, pick_number):
-    array = [1]
-    for index in range(array_size-1):
-        array.append(0)
-    for index in range(5000):
-        random.shuffle(array)
-    result = array[0] == 1
-    print_new_line("Consolation draw for pick {} is ".format(pick_number), " successful" if result else " not successful")
-    return result
-
-
-def get_consolation_prize_spot(first_pick, second_pick):
-    if second_pick == 0:
-        return first_pick
-    if consolation_prize_draw(2, first_pick):
-        return first_pick
-    if consolation_prize_draw(3, second_pick):
-        return second_pick
-    return 0
+    return consolation_prize_winner, (validate_number(arguments[2]))
 
 
 def reshuffle_divisions():
@@ -120,8 +91,7 @@ def reshuffle_divisions():
 
 
 def usage_fail():
-    print('usage: FantasyFootballDraw.py --reshuffle [<ConsolationPrizeWinner> <OnlyChoice>] | '
-          '[<ConsolationPrizeWinner> <FirstChoice> <SecondChoice>]\n')
+    print('usage: FantasyFootballDraw.py --reshuffle <ConsolationPrizeWinner> <OnlyChoice>')
     sys.exit(1)
 
 
@@ -140,24 +110,21 @@ def main():
     if should_reshuffle_divisions:
         reshuffle_divisions()
 
-    consolation_prize_winner = ''
-    consolation_prize_winner_final_spot = 0
+    arguments = [argument for argument in sys.argv if argument != '--reshuffle']
+    if len(arguments) != 3:
+        usage_fail()
 
-    arguments = [argument for argument in sys.argv if argument not in ['FantasyFootballDraw.py', '--reshuffle']]
-    if len(arguments) > 0:
-        consolation_prize_winner, consolation_prize_winner_first_pick, consolation_prize_winner_second_pick = \
-            validate_raw_input(arguments)
-        consolation_prize_winner_final_spot = \
-            get_consolation_prize_spot(consolation_prize_winner_first_pick, consolation_prize_winner_second_pick)
+    consolation_prize_winner, consolation_prize_winner_pick = \
+        validate_raw_input(arguments)
 
-    if consolation_prize_winner_final_spot != 0:
+    if consolation_prize_winner_pick != 0:
         players.remove(consolation_prize_winner)
 
     for index in range(5000):
         random.shuffle(players)
 
-    if consolation_prize_winner_final_spot != 0:
-        players.insert(consolation_prize_winner_final_spot-1, consolation_prize_winner)
+    if consolation_prize_winner_pick != 0:
+        players.insert(consolation_prize_winner_pick-1, consolation_prize_winner)
 
     print_results()
 
